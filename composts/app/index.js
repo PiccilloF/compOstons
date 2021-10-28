c96023c834fdb3c7bcd54838034ff12a253fff3e
Loïc <loicfort@free>
2021-10-28 17:39:06 +0200

    router id(\d+)

--- composts/app/index.js
+++ composts/app/index.js
@@ -7,6 +7,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+
 app.use(router);
 
 app.listen(PORT, () => {
--- composts/app/router.js
+++ composts/app/router.js
@@ -8,12 +8,12 @@ const router = Router();
 router
     .get('/', compostController.homePage)
     .get('/composts', compostController.getAllComposts)
-    .get('/composts/:id(//d+)', compostController.getOneCompost);
+    .get('/composts/:id(\\d+)', compostController.getOneCompost);
 
 
 router
     .get('/users', userController.getAllUsers)
-    .get('/users/:id(//d+)', userController.getOneUser);
+    .get('/users/:id(\\d+)', userController.getOneUser);
 
 
 module.exports = router;
