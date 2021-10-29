40e952b7ff57eb22f6d96e795e51018527f7fdc9
Loïc <loicfort@free>
2021-10-29 10:59:50 +0200

    route post + cross origin

--- composts/app/index.js
+++ composts/app/index.js
@@ -4,9 +4,7 @@ const router = require('./router');
 const app = express();
 const cors = require ('cors')
 
-app.use(cors({
-    origin:['https://compostons.herokuapp.com/', 'https://compostons-project.surge.sh']
-}));
+app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
