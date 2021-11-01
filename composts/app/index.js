1d24b1de6079b556fb5cd02edd2eeae61f34f0a8
Loïc <loicfort@free>
2021-11-01 10:39:20 +0100

    authoController

--- composts/app/index.js
+++ composts/app/index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const session = require("express-session");
 
 
-app.use(cors('*');
+app.use(cors('*'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
