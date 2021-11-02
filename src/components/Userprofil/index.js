07278cccf9bad952761d7c006c23a55a3a6990bf
PiccilloF <piccillofaustino@gmail.com>
2021-11-02 18:53:29 +0100

    css de base et test récupération de la liste via input geocontrol

--- src/components/Userprofil/index.js
+++ src/components/Userprofil/index.js
@@ -10,11 +10,31 @@ import axios from 'axios';
 // Immport du devTool de useForm hook, installé dans les devs depedencies.
 import { DevTool } from '@hookform/devtools';
 
+import './style.scss';
+
 // import { ErrorMessage } from '@hookform/error-message';
 
 export default function Userprofil() {
   const { register, handleSubmit, control } = useForm();
   const [coordinatesValue, setCoordinatesValue] = useState('');
+  const [adressInfo, setAdressInfo] = useState([]);
+
+  const provider = new OpenStreetMapProvider();
+
+  // test geoControl
+  const searchInput = async (value) => {
+    try {
+      const results = await provider.search({ query: value });
+      setAdressInfo(results);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  };
+
+  const listResults = adressInfo.map((item) => item.label);
+  const shortList = listResults.slice(5);
+  console.log(shortList);
 
   const onSubmit = (data) => {
     console.log(data);
@@ -40,83 +60,86 @@ export default function Userprofil() {
           className="main-profil__form"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div className="user-infos">
-            <div className="user-input-group">
-              <label htmlFor="firstname">Prénom: </label>
-              <input
-                className="user-input__element"
-                type="text"
-                id="firstname"
-                name="firstname"
-                {...register('firstname', { required: 'Veuillez saisir votre prénom' })}
-              />
-              <label htmlFor="lastname">Nom: </label>
-              <input
-                className="user-input__element"
-                type="text"
-                id="lastname"
-                name="lastname"
-                {...register('lastname')}
-              />
-              <label htmlFor="alias">Pseudo: </label>
-              <input
-                className="user-input__element"
-                type="text"
-                id="alias"
-                name="alias"
-                {...register('alias')}
-              />
-              <button
-                type="button"
-                onClick={() => console.log('je supprime mon compte')}
-                id="delete-profil__button"
-              >supprimer ce compte
-              </button>
-              <div className="profil-picture__container">
-                <div className="profil-picture">mon image</div>
-                <input type="file" id="picture-input" />
+          <div className="profil-picture__container">
+            <div className="profil-picture">mon image</div>
+            <input type="file" id="picture-input" />
+          </div>
+          <div className="infos-container">
+            <div className="user-infos">
+              <div className="user-input-group">
+                <label htmlFor="firstname">Prénom: </label>
+                <input
+                  className="user-input__element"
+                  type="text"
+                  id="firstname"
+                  name="firstname"
+                  {...register('firstname', { required: 'Veuillez saisir votre prénom' })}
+                />
+                <label htmlFor="lastname">Nom: </label>
+                <input
+                  className="user-input__element"
+                  type="text"
+                  id="lastname"
+                  name="lastname"
+                  {...register('lastname')}
+                />
+                <label htmlFor="alias">Pseudo: </label>
+                <input
+                  className="user-input__element"
+                  type="text"
+                  id="alias"
+                  name="alias"
+                  {...register('alias')}
+                />
+                <button
+                  type="button"
+                  onClick={() => console.log('je supprime mon compte')}
+                  id="delete-profil__button"
+                >supprimer ce compte
+                </button>
               </div>
-              <div className="compostType-infos">
-                <div className="compostType-checkbox-group">
-                  <label htmlFor="greenType">Déchets verts</label>
-                  <input
-                    className="compostType-checkbox-element"
-                    type="radio"
-                    name="compostType"
-                    id="vert"
-                    value="vert"
-                    {...register('compostType')}
-                  />
-                  <label htmlFor="greenType">Déchets bruns</label>
-                  <input
-                    className="compostType-checkbox-element"
-                    type="radio"
-                    name="compostType"
-                    id="marron"
-                    value="marron"
-                    {...register('compostType')}
-                  />
-                  <label htmlFor="trashType">Tous types de déchets compostables</label>
-                  <input
-                    className="compostType-checkbox-element"
-                    type="radio"
-                    name="compostType"
-                    id="tous types"
-                    value="tous types"
-                    {...register('compostType')}
-                  />
-                  <label htmlFor="availability">Je n'accepte pas de déchets en ce moment</label>
-                  <input
-                    className="compostType-checkbox-element"
-                    type="radio"
-                    name="compostType"
-                    id="aucun"
-                    value="aucun"
-                    {...register('compostType')}
-                  />
-                </div>
-                <div className="compost-inputs-block">
-                  <label htmlFor="address">Adresse: </label>
+            </div>
+            <div className="compostType-infos">
+              <div className="compostType-checkbox-group">
+                <label htmlFor="greenType">Déchets verts</label>
+                <input
+                  className="compostType-checkbox-element"
+                  type="radio"
+                  name="compostType"
+                  id="vert"
+                  value="vert"
+                  {...register('compostType')}
+                />
+                <label htmlFor="greenType">Déchets bruns</label>
+                <input
+                  className="compostType-checkbox-element"
+                  type="radio"
+                  name="compostType"
+                  id="marron"
+                  value="marron"
+                  {...register('compostType')}
+                />
+                <label htmlFor="trashType">Tous types de déchets compostables</label>
+                <input
+                  className="compostType-checkbox-element"
+                  type="radio"
+                  name="compostType"
+                  id="tous types"
+                  value="tous types"
+                  {...register('compostType')}
+                />
+                <label htmlFor="availability">Je n'accepte pas de déchets en ce moment</label>
+                <input
+                  className="compostType-checkbox-element"
+                  type="radio"
+                  name="compostType"
+                  id="aucun"
+                  value="aucun"
+                  {...register('compostType')}
+                />
+              </div>
+              <div className="compost-inputs-block">
+                {/* <label htmlFor="address">Adresse: </label>
                   <input
                     className="user-input__element"
                     type="text"
@@ -139,11 +162,15 @@ export default function Userprofil() {
                     id="city"
                     name="city"
                     {...register('city')}
-                  />
-                </div>
+                  /> */}
+                <input
+                  type="text"
+                  onChange={(event) => searchInput(event.target.value)}
+                />
               </div>
             </div>
           </div>
+
           <button
             id="register-profil__button"
             type="submit"
