f0c3850deaf16e24a4fbaefaf4043a67d5ead6e3
Loïc <loicfort@free>
2021-11-04 11:06:08 +0100

    logout

--- composts/app/index.js
+++ composts/app/index.js
@@ -7,13 +7,15 @@ const cors = require('cors');
 const session = require("express-session");
 
 // allow all origin cors for now - to do : allow only specific ones
-app.use(cors('*'));
+// app.use(cors('*'));
 
-// app.all('/', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     next()
-//   });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
